486998e385ee2fbd96f3a781dfb96b80841726e3
Suryansh Pandey <coderhood0011@gmail.com>
2025-04-14 22:25:29 +0530

    Controller for uploading user defined data configured

--- server/src/controllers/jsonProcessor.controller.js
+++ server/src/controllers/jsonProcessor.controller.js
@@ -22,3 +22,46 @@ export const getFileData = asyncHandler(async (req, res, next) => {
     }
 });
 
+export const uploadFileData = asyncHandler(async (req, res, next) => {
+    try {
+        const fileData = req.body.jsonData; // getting the file data from the request body
+
+        //validating the file data
+        if (!fileData || fileData.length === 0) {
+            // checking if the file data is empty or not
+            throw new ApiError(400, "File data is required");
+        }
+
+        // checking if the file data is in valid json format or not
+        let  jsonData = null;
+        try {
+            jsonData = JSON.parse(fileData); // parsing the file data to json format
+        } catch (error) {
+            throw new ApiError(400, "File is not in valid json format : " + error.message);
+        }
+
+        // checking if the json data is empty or not
+        if (!jsonData || Object.keys(jsonData).length === 0) {
+            throw new ApiError(404, "File is empty or not in valid format");
+        }
+
+        // writing the new file data to the file
+        fs.writeFileSync(
+            `./public/${process.env.DEFAULT_USER_FILE_NAME}`, // file to be created or overwritten
+            JSON.stringify(jsonData, null, 2), // converting the json data to string format and adding indentation of 2 spaces
+            "utf-8"
+        );
+
+        // sending success response
+        return res.status(200).json(new ApiResponse(200, jsonData, "File data uploaded successfully"));
+
+    } catch (error) {
+        return res.status(error.statusCode || 500).json(
+            new ApiResponse(
+                error.statusCode || 500,
+                null,
+                error.message || "Something went wrong"
+            )
+        )
+    }
+});
--- server/src/routes/dataVisualizer.routes.js
+++ server/src/routes/dataVisualizer.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { getFileData } from '../controllers/jsonProcessor.controller.js';
+import { getFileData, uploadFileData } from '../controllers/jsonProcessor.controller.js';
 import { passFileData } from '../middlewares/fileData.middleware.js';
 
 const router = Router();
 
 // Route to get the file data
 router.get('/file-data', passFileData, getFileData);
+router.post('/file-data', uploadFileData);
 
 export default router;
